17f3fb96a5993976e9b549d3e04175dac83c1b04
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-06-26 05:32:12 -0300

    create structs

--- src/libclifs.c
+++ src/libclifs.c
@@ -1,3 +1,63 @@
 #include "libclifs.h"
 
-// libclifs definitions goes here
+#include <math.h>
+
+void fillLocalStiffnessMatrix(double _matrix[12][12], Bar* _bar)
+{
+    double L  = _bar->L;
+    double E  = _bar->E;
+    double G  = _bar->G;
+    double A  = _bar->A;
+    double Iy = _bar->Iy;
+    double Iz = _bar->Iz;
+    double J  = _bar->J;
+
+    for(int i = 0; i < 12; i++)
+    {
+        for(int j = 0; j < 12; j++)
+        {
+            _matrix[i][j] = 0;
+        }
+    }
+        
+    _matrix[0][0]   = (E*A)/L;
+    _matrix[6][0]   = (-1)*((E*A)/L);
+    _matrix[1][1]   = (12*E*Iz)/pow(L,3);
+    _matrix[5][1]   = (6*E*Iz)/pow(L,2);
+    _matrix[7][1]   = (-1)*((12*E*Iz)/pow(L,3));
+    _matrix[11][1]  = (6*E*Iz)/pow(L,2);
+    _matrix[2][2]   = (12*E*Iy)/pow(L,3);
+    _matrix[4][2]   = (-1)*((6*E*Iy)/pow(L,2));
+    _matrix[8][2]   = (-1)*((12*E*Iy)/pow(L,3));
+    _matrix[10][2]  = (-1)*((6*E*Iy)/pow(L,2));
+    _matrix[3][3]   = (G*J)/L;
+    _matrix[9][3]   = (-1)*((G*J)/L);
+    _matrix[2][4]   = (-1)*((6*E*Iy)/pow(L,2));
+    _matrix[4][4]   = (4*E*Iy)/L;
+    _matrix[8][4]   = (6*E*Iy)/pow(L,2);
+    _matrix[10][4]  = (2*E*Iy)/L;
+    _matrix[1][5]   = (6*E*Iz)/pow(L,2);
+    _matrix[5][5]   = (4*E*Iz)/L;
+    _matrix[7][5]   = (-1)*((6*E*Iz)/pow(L,2));
+    _matrix[11][5]  = (2*E*Iz)/L;
+    _matrix[0][6]   = (-1)*((E*A)/L);
+    _matrix[6][6]   = (E*A)/L;
+    _matrix[1][7]   = (-1)*((12*E*Iz)/pow(L,3));
+    _matrix[5][7]   = (-1)*((6*E*Iz)/pow(L,2));
+    _matrix[7][7]   = (12*E*Iz)/pow(L,3);
+    _matrix[11][7]  = (-1)*((6*E*Iz)/pow(L,2));
+    _matrix[2][8]   = (-1)*((12*E*Iy)/pow(L,3));
+    _matrix[4][8]   = (6*E*Iy)/pow(L,2);
+    _matrix[8][8]   = (12*E*Iy)/pow(L,3);
+    _matrix[10][8]  = (6*E*Iy)/pow(L,2);
+    _matrix[3][9]   = (-1)*((G*J)/L);
+    _matrix[9][9]   = (G*J)/L;
+    _matrix[2][10]  = (-1)*((6*E*Iy)/pow(L,2));
+    _matrix[4][10]  = (2*E*Iy)/L;
+    _matrix[8][10]  = (6*E*Iy)/pow(L,2);
+    _matrix[10][10] = (4*E*Iy)/L;
+    _matrix[1][11]  = (6*E*Iz)/pow(L,2);
+    _matrix[5][11]  = (2*E*Iz)/L;
+    _matrix[7][11]  = (-1)*((6*E*Iz)/pow(L,2));
+    _matrix[11][11] = (4*E*Iz)/L;
+}
--- src/libclifs.h
+++ src/libclifs.h
@@ -1,10 +1,16 @@
-// HEADER GUARD (BEGIN) -----------------------------------------------------------
+// --------------------------------------------------------------------------------
+// LIBCLIFS - TIAGO MELO JUCA (2021)                                              |
+// --------------------------------------------------------------------------------
+// This library is for studying purposes only - please don't take it seriously :)
+// You'll see a lot of comments here. Yep, I know Clean Code, but, as I said, it's
+// just for learning; the purpose is a reference for myself, nothing more
+// --------------------------------------------------------------------------------
 
+// HEADER GUARD (BEGIN) -----------------------------------------------------------
 #ifndef LIBCLIFS_H
 #define LIBCLIFS_H
 
 // MUST BE DEFINED AT COMPILE TIME, BUT ONLY WHEN BUILDING THE DLL ITSELF ---------
-
 #ifdef _WIN32
     #ifdef BUILD_DLL
         #define CLIFS_API __declspec(dllexport)
@@ -16,21 +22,45 @@
 #endif
 
 // MAKE THIS HEADER FILE COMPATIBLE WITH C++ CODE TOO (BEGIN) ---------------------
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 // HEADER CONTENT -----------------------------------------------------------------
 
-// libclifs declarations goes here
+// REMINDER TO MYSELF (SOURCE-> www.tutorialspoint.com/cprogramming/c_typedef.htm)
+// By convention, uppercase letters are used for these definitions to
+// remind the user that the type name is really a symbolic abbreviation
+typedef unsigned char BYTE;
+
+// You can use typedef to give a name to your user defined data types as well
+// For example, you can use typedef with structure to define a new data type
+// and then use that data type to define structure variables directly
+// We could declare our struct first, and later just do as following:
+// typedef struct point Point;
+// But let's just do the short way...
+typedef struct point {
+    double x;
+    double y;
+    double z;
+} Point;
+
+typedef struct bar {
+    double L;
+    double E;
+    double G;
+    double A;
+    double Iy;
+    double Iz;
+    double J;
+} Bar;
+
+void fillLocalStiffnessMatrix(double _matrix[12][12], Bar* _bar);
 
 // MAKE THIS HEADER FILE COMPATIBLE WITH C++ CODE TOO (END) -----------------------
-
 #ifdef __cplusplus
 }
 #endif
 
 // HEADER GUARD (END) -------------------------------------------------------------
-
 #endif // LIBCLIFS_H
