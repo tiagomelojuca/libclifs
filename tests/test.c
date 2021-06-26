746d9e835e5735893fd44f755a39acaab4d07044
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-06-26 18:41:31 -0300

    create first test

---
+++ tests/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "../src/libclifs.h"
+
+// cmd /c "gcc tests/test.c -o build/a.exe && .\build\a"
+
+int main()
+{
+    Material concrete;
+    concrete.e = 100000;
+    concrete.g = 38462;
+
+    return 0;
+}
