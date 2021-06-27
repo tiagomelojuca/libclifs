f15657894ba354269c25395c51fb46d7e0009200
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-06-27 06:11:04 -0300

    remove old tests

--- test.bat
+++ test.bat
@@ -1,16 +1,6 @@
 @echo off
 echo.
 
-echo -- Testing elements.c --
-cmd /c "gcc src/libclifs.c tests/elements.c -o build/a.exe && .\build\a"
-echo.
-echo.
-
-echo -- Testing datastructs.c --
-cmd /c "gcc src/libclifs.c tests/datastructs.c -o build/a.exe && .\build\a"
-echo.
-echo.
-
 echo -- Testing example1.c --
 cmd /c "gcc src/libclifs.c tests/example1.c -o build/a.exe && .\build\a"
 echo.
