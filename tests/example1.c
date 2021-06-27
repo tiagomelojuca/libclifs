1fb50dc3a7d1e3134198556afbb607be7c51a1a4
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-06-27 02:13:37 -0300

    improve test example1

--- tests/example1.c
+++ tests/example1.c
@@ -6,16 +6,46 @@ int main()
     GlobalSystem g;
     initGlobalSystem(&g);
 
-    Node n1, n2;
-    n1.position.x = 5.0;
-    n2.position.y = 7.0;
+    Material concrete = createMaterial(100000.0, 38462.0);
+    Section rectangle = createSection(0.01, 0.00001, 0.00001, 0.00001);
+
+    DegreesOfFreedom allFree = createDegreesOfFreedom(true, true, true);
+    DegreesOfFreedom allFix = createDegreesOfFreedom(false, false, false);
+
+    NodalLoad l1 = createNodalLoad(0.0,  0.0,   0.0, 0.0,     0.0, 0.0);
+    NodalLoad l2 = createNodalLoad(8.9,  0.0,   0.0, 0.0,     0.0, 0.0);
+    NodalLoad l3 = createNodalLoad(0.0, 17.8,   0.0, 0.0,     0.0, 0.0);
+    NodalLoad l4 = createNodalLoad(0.0,  0.0, -4.45, 0.0, -10.858, 0.0);
+    NodalLoad l5 = createNodalLoad(0.0,  0.0,   0.0, 0.0,     0.0, 0.0);
+
+    Node n1 = createNode(createPoint( 0.0,  0.0,  0.0), allFix,  allFix,  l1);
+    Node n2 = createNode(createPoint( 0.0,  0.0, 2.44), allFree, allFree, l2);
+    Node n3 = createNode(createPoint(2.44,  0.0, 2.44), allFree, allFree, l3);
+    Node n4 = createNode(createPoint(4.88,  0.0, 2.44), allFree, allFree, l4);
+    Node n5 = createNode(createPoint(7.32, 2.44,  9.0), allFix,  allFix,  l5);
     
     insertNodeGlobalSystem(&g, n1);
     insertNodeGlobalSystem(&g, n2);
+    insertNodeGlobalSystem(&g, n3);
+    insertNodeGlobalSystem(&g, n4);
+    insertNodeGlobalSystem(&g, n5);
 
-    printf("%f\n", g.nodeArray.nodes[1].position.y);
-    printf("%i\n", g.nodeArray.used);
-    printf("all fine");
+    Point auxvec = createPoint(0.0, 0.707, 0.707);
+    FrameBar b1 = createFrameBar(n1, n2, auxvec, &concrete, &rectangle);
+    FrameBar b2 = createFrameBar(n2, n3, auxvec, &concrete, &rectangle);
+    FrameBar b3 = createFrameBar(n3, n4, auxvec, &concrete, &rectangle);
+    FrameBar b4 = createFrameBar(n4, n5, auxvec, &concrete, &rectangle);
+
+    insertFrameBarGlobalSystem(&g, b1);
+    insertFrameBarGlobalSystem(&g, b2);
+    insertFrameBarGlobalSystem(&g, b3);
+    insertFrameBarGlobalSystem(&g, b4);
+
+    printf("Number of Nodes: %i\n", g.nodeArray.used);
+    printf("Number of FrameBars: %i\n", g.framebarsArray.used);
+    printf("Number of Equations: %i\n", g.numEquations);
+    printf("Number of Freedoms: %i\n", g.numEqFreedoms);
+    printf("Number of Constraints: %i\n", g.numEqConstraint);
 
     freeGlobalSystem(&g);
     return 0;
