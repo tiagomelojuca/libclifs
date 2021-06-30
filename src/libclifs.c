5d44208e94f456b5d82f794202be65793a76192a
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-06-30 14:02:41 -0300

    missing line breaks

--- src/libclifs.c
+++ src/libclifs.c
@@ -332,6 +332,7 @@ void _fillGlobalStiffnessMatrix(double _matrix[SM][SM], double _rotation[SM][SM]
                                 double _tRotation[SM][SM], double _local[SM][SM])
 {
     double product[12][12] = { 0 };
+    
     for(int i = 0; i < SM; i++) {
         for(int j = 0; j < SM; j++) {
             for(int k = 0; k < SM; k++) {
