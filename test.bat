fc0ddc2565b18a3b36cd2d83083af3c2e40975c4
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-06-30 20:15:22 -0300

    add gdb debugging

--- test.bat
+++ test.bat
@@ -2,6 +2,6 @@
 echo.
 
 echo -- Testing example1.c --
-cmd /c "gcc src/libclifs.c src/utils.c tests/example1.c -o build/a.exe && .\build\a"
+cmd /c "gcc -g src/libclifs.c src/utils.c tests/example1.c -o build/a.exe && .\build\a"
 echo.
 echo.
