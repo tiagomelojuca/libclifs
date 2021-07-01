f88f0dfd94e04b70e03cf00da0595c2531675231
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-06-30 21:28:24 -0300

    finish example 1

--- tests/example1.c
+++ tests/example1.c
@@ -1,47 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "../src/libclifs.h"
+#include "../src/utils.h"
 
-void printDoubleMatrix(double** matrix, int nRows, int nColumns)
-{
-    for(int i = 0; i < nRows; i++) {
-        for(int j = 0; j < nColumns; j++) {
-            printf("%.1f  ", matrix[i][j]);
-        } printf("\n");
-    }
-}
+#define A  0.01
+#define I  0.00001
+#define E  100000000.0
+#define G  38462000.0
 
-void printIntMatrix(int** matrix, int nRows, int nColumns)
-{
-    for(int i = 0; i < nRows; i++) {
-        for(int j = 0; j < nColumns; j++) {
-            printf("%d  ", matrix[i][j]);
-        } printf("\n");
-    }
-}
+#define VX 0.000
+#define VY 0.707
+#define VZ 0.707
 
 int main()
-{
+{   
     GlobalSystem g;
     initGlobalSystem(&g);
 
-    Material concrete = createMaterial(100000.0, 38462.0);
-    Section rectangle = createSection(0.01, 0.00001, 0.00001, 0.00001);
+    Point auxvec = createPoint(VX, VY, VZ);
+    Material concrete = createMaterial(E, G);
+    Section rectangle = createSection(A, I, I, I);
 
     DegreesOfFreedom allFree = createDegreesOfFreedom(true, true, true);
     DegreesOfFreedom allFix = createDegreesOfFreedom(false, false, false);
 
-    NodalLoad l1 = createNodalLoad(0.0,  0.0,   0.0, 0.0,     0.0, 0.0);
-    NodalLoad l2 = createNodalLoad(8.9,  0.0,   0.0, 0.0,     0.0, 0.0);
-    NodalLoad l3 = createNodalLoad(0.0, 17.8,   0.0, 0.0,     0.0, 0.0);
-    NodalLoad l4 = createNodalLoad(0.0,  0.0, -4.45, 0.0, -10.858, 0.0);
-    NodalLoad l5 = createNodalLoad(0.0,  0.0,   0.0, 0.0,     0.0, 0.0);
+    NodalLoad l1 = createNodalLoad(0.000,  0.000,   0.000,  0.000,  0.000,  0.000);
+    NodalLoad l2 = createNodalLoad(8.900,  0.000,   0.000,  0.000,  0.000,  0.000);
+    NodalLoad l3 = createNodalLoad(0.000, 17.800,   0.000,  0.000,  0.000,  0.000);
+    NodalLoad l4 = createNodalLoad(0.000,  0.000,  -4.450,  0.000,-10.858,  0.000);
+    NodalLoad l5 = createNodalLoad(0.000,  0.000,   0.000,  0.000,  0.000,  0.000);
 
-    Node n1 = createNode(createPoint( 0.0,  0.0,  0.0), allFix,  allFix,  l1);
-    Node n2 = createNode(createPoint( 0.0,  0.0, 2.44), allFree, allFree, l2);
-    Node n3 = createNode(createPoint(2.44,  0.0, 2.44), allFree, allFree, l3);
-    Node n4 = createNode(createPoint(4.88,  0.0, 2.44), allFree, allFree, l4);
-    Node n5 = createNode(createPoint(7.32, 2.44,  9.0), allFix,  allFix,  l5);
+    Node n1 = createNode(createPoint(0.000,  0.000,  0.000), allFix,  allFix,  l1);
+    Node n2 = createNode(createPoint(0.000,  0.000,  2.440), allFree, allFree, l2);
+    Node n3 = createNode(createPoint(2.440,  0.000,  2.440), allFree, allFree, l3);
+    Node n4 = createNode(createPoint(4.880,  0.000,  2.440), allFree, allFree, l4);
+    Node n5 = createNode(createPoint(7.320,  2.440,  0.000), allFix,  allFix,  l5);
     
     insertNodeGlobalSystem(&g, n1);
     insertNodeGlobalSystem(&g, n2);
@@ -49,34 +43,27 @@ int main()
     insertNodeGlobalSystem(&g, n4);
     insertNodeGlobalSystem(&g, n5);
 
-    Point auxvec = createPoint(0.0, 0.707, 0.707);
-    FrameBar b1 = createFrameBar(&g.nodeArray.nodes[0], &g.nodeArray.nodes[1], auxvec, &concrete, &rectangle);
-    FrameBar b2 = createFrameBar(&g.nodeArray.nodes[1], &g.nodeArray.nodes[2], auxvec, &concrete, &rectangle);
-    FrameBar b3 = createFrameBar(&g.nodeArray.nodes[2], &g.nodeArray.nodes[3], auxvec, &concrete, &rectangle);
-    FrameBar b4 = createFrameBar(&g.nodeArray.nodes[3], &g.nodeArray.nodes[4], auxvec, &concrete, &rectangle);
+    FrameBar b1 = createFrameBar(&g.nodeArray.nodes[0], &g.nodeArray.nodes[1],
+                                 auxvec, &concrete, &rectangle);
+    FrameBar b2 = createFrameBar(&g.nodeArray.nodes[1], &g.nodeArray.nodes[2],
+                                 auxvec, &concrete, &rectangle);
+    FrameBar b3 = createFrameBar(&g.nodeArray.nodes[2], &g.nodeArray.nodes[3],
+                                 auxvec, &concrete, &rectangle);
+    FrameBar b4 = createFrameBar(&g.nodeArray.nodes[3], &g.nodeArray.nodes[4],
+                                 auxvec, &concrete, &rectangle);
 
     insertFrameBarGlobalSystem(&g, b1);
     insertFrameBarGlobalSystem(&g, b2);
     insertFrameBarGlobalSystem(&g, b3);
     insertFrameBarGlobalSystem(&g, b4);
 
-    printf("\n");
-    printf("Number of Nodes: %i\n", g.nodeArray.used);
-    printf("Number of FrameBars: %i\n", g.framebarsArray.used);
-    printf("Number of Equations: %i\n", g.numEquations);
-    printf("Number of Freedoms: %i\n", g.numEqFreedoms);
-    printf("Number of Constraints: %i\n", g.numEqConstraints);
-    printf("\n");
-
     mountGlobalSystem(&g);
-    // printMatrix(g.mtxDisplacements, g.numEquations, 1); printf("\n");
-    // printIntMatrix(g.mtxConstraints, DOG, g.nodeArray.used); printf("\n");
-    // printIntMatrix(g.mtxFreedoms, DOG, g.nodeArray.used); printf("\n");
-    // printIntMatrix(g.mtxSpreading, g.framebarsArray.used, SM); printf("\n");
-    printDoubleMatrix(g.mtxStiffness, g.numEquations, g.numEquations); printf("\n");
-    // printDoubleMatrix(g.mtxNodalLoads, g.numEquations, 1); printf("\n");
 
-    printf("Done.\n");
+    printf("SUPPORT REACTIONS:\n");
+    for(int i = 0; i < g.numEqConstraints; i++) {
+        printf("%.2f\n", g.vecSupportReactions[i][0]);
+    }
+
     freeGlobalSystem(&g);
     return 0;
 }
