db042e57e02f28ca70d21902f94aaef953b34e60
Tiago Melo Juca <tiagomelojuca@gmail.com>
2021-09-25 00:57:18 -0300

    add disclaimer to all src files

--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,11 @@
+// --------------------------------------------------------------------------------
+// LIBCLIFS - TIAGO MELO JUCA (2021)                                              |
+// --------------------------------------------------------------------------------
+// This library is for studying purposes only - please don't take it seriously :)
+// You'll see a lot of comments here. Yep, I know Clean Code, but, as I said, it's
+// just for learning; the purpose is a reference for myself, nothing more
+// --------------------------------------------------------------------------------
+
 #ifndef UTILS_H
 #define UTILS_H
 
